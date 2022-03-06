29fd8c5db3e0acbb4d21651b2be660f3d850e590
Bernhard Mühl <bernhard.muehl@vbcf.ac.at>
2022-03-06 16:30:43 +0100

    Add static adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
-import adapter from '@sveltejs/adapter-auto';
-import preprocess from 'svelte-preprocess';
+import adapter from "@sveltejs/adapter-static";
+import preprocess from "svelte-preprocess";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -8,7 +8,14 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
-		adapter: adapter()
+		prerender: {
+			default: true
+		},
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: null,
+		}),
 	}
 };
 
