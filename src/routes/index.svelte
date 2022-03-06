220de5ba9744f772ddbee4d7376f08ae44c61f54
Bernhard Mühl <bernhard.muehl@vbcf.ac.at>
2022-03-06 17:32:29 +0100

    Experiments with svelte cubed

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,15 +1,40 @@
 <script lang="ts">
 	import { scrollTo, scrollRef } from "svelte-scrolling";
+	import * as THREE from "three";
+	import * as SC from "svelte-cubed";
+
+	let spin = 0;
+
+	SC.onFrame(() => {
+		spin += 0.01;
+	});
 </script>
 
 <section use:scrollRef={"home"}>
-	<div>
-		<h1>
-			Hi,<br />
-			I'm Bernhard!
-		</h1>
-		<p>Full Stack Developer / Software Architect</p>
-		<a href="#about" use:scrollTo={"about"}>next</a>
+	<div class="flex flex-col">
+		<div>
+			<h1>
+				Hi,<br />
+				I'm Bernhard!
+			</h1>
+			<p>Full Stack Developer / Software Architect</p>
+			<a href="#about" use:scrollTo={"about"}>next</a>
+		</div>
+
+		<div class="flex justify-center relative h-44">
+			​<SC.Canvas antialias alpha>
+				<SC.Mesh
+					geometry={new THREE.BoxGeometry()}
+					material={new THREE.MeshStandardMaterial({ color: 0xb48ead })}
+					rotation={[0, spin, 0]}
+				/>
+				<SC.PerspectiveCamera position={[1, 1, 3]} zoom={1.5} />
+				<SC.OrbitControls enableZoom={false} />
+				<SC.AmbientLight intensity={0.75} />
+				<SC.DirectionalLight intensity={0.6} position={[-2, 3, 2]} />
+				​</SC.Canvas
+			>
+		</div>
 	</div>
 </section>
 <section use:scrollRef={"about"}>
