e3115a41a436d9a09897900238bf13e93219790e
Bernhard Mühl <bernhard.muehl@vbcf.ac.at>
2022-03-08 00:35:34 +0100

    Extract sections to components

---
+++ src/lib/intro.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	import * as THREE from "three";
+	import * as SC from "svelte-cubed";
+
+	let spin = 0;
+
+	SC.onFrame(() => {
+		spin += 0.01;
+	});
+</script>
+
+<div class="flex flex-col">
+	<div>
+		<h1>
+			Hi,<br />
+			I'm Bernhard!
+		</h1>
+		<p>Full Stack Developer / Software Architect</p>
+	</div>
+
+	<div class="flex justify-center relative h-44">
+		​<SC.Canvas antialias alpha>
+			<SC.Mesh
+				geometry={new THREE.BoxGeometry()}
+				material={new THREE.MeshStandardMaterial({ color: 0xb48ead })}
+				rotation={[0, spin, spin + 0.04]}
+			/>
+			<SC.PerspectiveCamera position={[1, 1, 3]} zoom={1.5} />
+			<SC.OrbitControls enableZoom={false} />
+			<SC.AmbientLight intensity={0.75} />
+			<SC.DirectionalLight intensity={0.6} position={[-2, 3, 2]} />
+		</SC.Canvas>
+	</div>
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,46 +1,31 @@
 <script lang="ts">
 	import { scrollTo, scrollRef } from "svelte-scrolling";
-	import * as THREE from "three";
-	import * as SC from "svelte-cubed";
+	import Intro from "$lib/intro.svelte";
+	import Experience from "$lib/experience.svelte";
+	import Skills from "$lib/skills.svelte";
+	import Private from "$lib/private.svelte";
 
-	let spin = 0;
-
-	SC.onFrame(() => {
-		spin += 0.01;
-	});
+	const sections = [
+		{ name: "intro", component: Intro },
+		{ name: "experience", component: Experience },
+		{ name: "skills", component: Skills },
+		{ name: "private", component: Private },
+	];
 </script>
 
-<section use:scrollRef={"home"}>
-	<div class="flex flex-col">
-		<div>
-			<h1>
-				Hi,<br />
-				I'm Bernhard!
-			</h1>
-			<p>Full Stack Developer / Software Architect</p>
-			<a href="#about" use:scrollTo={"about"}>next</a>
-		</div>
+{#each sections as section, index (index)}
+	<section use:scrollRef={section.name}>
+		<svelte:component this={section.component} />
 
-		<div class="flex justify-center relative h-44">
-			​<SC.Canvas antialias alpha>
-				<SC.Mesh
-					geometry={new THREE.BoxGeometry()}
-					material={new THREE.MeshStandardMaterial({ color: 0xb48ead })}
-					rotation={[0, spin, 0]}
-				/>
-				<SC.PerspectiveCamera position={[1, 1, 3]} zoom={1.5} />
-				<SC.OrbitControls enableZoom={false} />
-				<SC.AmbientLight intensity={0.75} />
-				<SC.DirectionalLight intensity={0.6} position={[-2, 3, 2]} />
-				​</SC.Canvas
-			>
-		</div>
-	</div>
-</section>
-<section use:scrollRef={"about"}>
-	<h1>About</h1>
-	<a href="#home" use:scrollTo={"home"}>got to top</a>
-</section>
+		{#if index === sections.length - 1}
+			<!-- svelte-ignore a11y-missing-attribute -->
+			<a class="mt-6" use:scrollTo={sections[0].name}>go to top</a>
+		{:else}
+			<!-- svelte-ignore a11y-missing-attribute -->
+			<a class="mt-6" use:scrollTo={sections[index + 1].name}>next</a>
+		{/if}
+	</section>
+{/each}
 
 <style lang="postcss">
 	section {
